51ca96710a0b2b270f942ebf347351e2c6a40923
grant-me-the-sweet-release-of-death <154357563+grant-me-the-sweet-release-of-death@users.noreply.github.com>
2025-02-14 01:19:34 +0300

    Update logout.php

--- app/admin/logout.php
+++ app/admin/logout.php
@@ -1,8 +1,7 @@
 <?php
 require_once '../core/init.php'; 
 
-// Выход из системы
 Eshop::logOut();
 
-header('Location: /enter.php'); // Переадресация на страницу входа после выхода 
+header('Location: /enter.php'); 
 exit();
