809f3cebe65d110064f5e642d306413a1ba0d957
grant-me-the-sweet-release-of-death <154357563+grant-me-the-sweet-release-of-death@users.noreply.github.com>
2025-02-14 02:30:10 +0300

    Update enter.php

--- app/enter.php
+++ app/enter.php
@@ -3,34 +3,31 @@ require_once '../core/init.php';
 
 try {
    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-       // Получаем данные из формы логина
        $login = Cleaner::str($_POST['login']);
        $password = Cleaner::str($_POST['password']);
        
-       // Создаем объект User для проверки логина и пароля
        $user = new User($login, password_hash('', PASSWORD_DEFAULT)); 
        if (Eshop::logIn($user)) { 
-           header('Location: /admin'); // Переадресация на страницу админки после успешного входа 
+           header('Location: /admin'); 
            exit(); 
        }
    }
 } catch (Exception $e) { 
-   echo 'Ошибка: ' . htmlspecialchars($e->getMessage()); 
+   echo 'Error: ' . htmlspecialchars($e->getMessage()); 
 }
 ?>
 
-<!-- HTML форма для входа -->
-<h1>Вход в админку</h1>
+<h1>Admin console</h1>
 <form action="login.php" method="post">
    <div>
-       <label>Логин:</label>
+       <label>Login:</label>
        <input type="text" name="login" required>
    </div>
    <div>
-       <label>Пароль:</label>
+       <label>Password:</label>
        <input type="password" name="password" required>
    </div>
    <div>
-       <input type="submit" value="Войти">
+       <input type="submit" value="Login">
    </div>
 </form>
