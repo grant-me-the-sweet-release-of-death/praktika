b20ec179354c514bf8d43f47725a5799328f312d
grant-me-the-sweet-release-of-death <154357563+grant-me-the-sweet-release-of-death@users.noreply.github.com>
2025-02-14 01:15:17 +0300

    Update admin.php

--- app/admin/admin.php
+++ app/admin/admin.php
@@ -1,9 +1,9 @@
-<h1>Администрирование магазина</h1>
-<h3>Доступные действия:</h3>
+<h1>Store administration</h1>
+<h3>Available actions:</h3>
 <ul>
-    <li><a href='catalog'>Каталог товаров</a></li>
-    <li><a href='admin/add_item_to_catalog'>Добавление товара в каталог</a></li>
-    <li><a href='admin/orders'>Просмотр готовых заказов</a></li>
-    <li><a href='admin/create_user'>Добавить пользователя</a></li>
-    <li><a href='admin/logout'>Завершить сеанс</a></li>
+    <li><a href='catalog'>Product catalog</a></li>
+    <li><a href='admin/add_item_to_catalog'>Add a product to catalog</a></li>
+    <li><a href='admin/orders'>List of ready orders</a></li>
+    <li><a href='admin/create_user'>Add user</a></li>
+    <li><a href='admin/logout'>Finish session</a></li>
 </ul>
