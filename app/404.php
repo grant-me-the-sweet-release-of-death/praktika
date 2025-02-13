cbb712c5f1a261b6d2057d9099fff502ae326eca
grant-me-the-sweet-release-of-death <154357563+grant-me-the-sweet-release-of-death@users.noreply.github.com>
2025-02-14 02:04:24 +0300

    Update 404.php

--- app/404.php
+++ app/404.php
@@ -2,5 +2,5 @@
     header('Refresh: 3, url=/catalog');
 ?>
 <h1>404</h1>
-<h3>По этому адресу ничего нет :(</h3>
-<p>Через 3 секунды вы будете переадресованы на каталог товаров.</p>
+<h3>Nothing found by this address :(</h3>
+<p>In 3 seconds, you will be brought back to the catalog.</p>
