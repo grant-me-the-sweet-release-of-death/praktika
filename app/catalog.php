a4ea14df4d0edcc6ef018b68dc3386f4f36a483d
grant-me-the-sweet-release-of-death <154357563+grant-me-the-sweet-release-of-death@users.noreply.github.com>
2025-02-14 02:26:32 +0300

    Update catalog.php

--- app/catalog.php
+++ app/catalog.php
@@ -1,37 +1,35 @@
 <?php
-require_once 'core/init.php'; // Подключаем файл инициализации
+require_once 'core/init.php'; 
 
 ?>
 
-<h1>Каталог товаров</h1>
-<p class='admin'><a href='admin'>админка</a></p>
-<p>Товаров в <a href='basket'>корзине</a>: </p>
+<h1>Product catalog</h1>
+<p class='admin'><a href='admin'>Admin</a></p>
+<p>Products in <a href='basket'>basket</a>: </p>
 <table>
 <tr>
-    <th>Название</th>
-    <th>Автор</th>
-    <th>Год издания</th>
-    <th>Цена, руб.</th>
-    <th>В корзину</th>
+    <th>Name</th>
+    <th>Author</th>
+    <th>Publication date</th>
+    <th>Cost, rub.</th>
+    <th>Add to checkout</th>
 </tr>
 
 <?php
 try {
-    // Получаем итератор книг из каталога
     $booksIterator = Eshop::getItemsFromCatalog();
 
-    // Заполняем таблицу данными о книгах
     foreach ($booksIterator as $book) {
         echo "<tr>";
         echo "<td>" . htmlspecialchars($book->getTitle()) . "</td>";
         echo "<td>" . htmlspecialchars($book->getAuthor()) . "</td>";
         echo "<td>" . htmlspecialchars($book->getPubyear()) . "</td>";
-        echo "<td>" . htmlspecialchars(number_format($book->getPrice(), 2, '.', ' ')) . " руб.</td>";
-        echo "<td><button onclick=\"addToBasket('" . htmlspecialchars($book->getTitle()) . "')\">В корзину</button></td>"; // Кнопка для добавления в корзину
+        echo "<td>" . htmlspecialchars(number_format($book->getPrice(), 2, '.', ' ')) . " rub.</td>";
+        echo "<td><button onclick=\"addToBasket('" . htmlspecialchars($book->getTitle()) . "')\">Add to checkout</button></td>"; 
         echo "</tr>";
     }
 } catch (Exception $e) {
-    echo "<tr><td colspan='5'>Ошибка: " . htmlspecialchars($e->getMessage()) . "</td></tr>";
+    echo "<tr><td colspan='5'>Error: " . htmlspecialchars($e->getMessage()) . "</td></tr>";
 }
 ?>
 
